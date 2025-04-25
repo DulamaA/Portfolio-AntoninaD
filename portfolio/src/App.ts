a4bc7a9cdf11f41326e7a5406e330695bb90b0aa
DulamaA <114867084+DulamaA@users.noreply.github.com>
2025-04-25 16:07:31 +0200

    built the section projects and styled

--- portfolio/src/App.ts
+++ portfolio/src/App.ts
@@ -27,42 +27,65 @@ const App = (): HTMLElement => {
   aboutSection.id = 'about';
   aboutSection.innerHTML = `
   <h2>About Me</h2>
+  <div class= "about-content">
+  <img src="/public/images/profile.jpg" alt="Antonina Dulama" class="profile-img"/>
   <p>Lorem ipsilom</p>
+  </div>
   `;
   container.appendChild(aboutSection);
 
   //PROJECTS section
-  const projects = document.createElement('section');
-  projects.id = 'projects';
-  projects.innerHTML = '<h2>Projects</h2>';
+  const projectsGrid = document.createElement('section');
+  projectsGrid.id = 'projects';
+  projectsGrid.className = 'project-grid';
+  projectsGrid.innerHTML = '<h2>Projects</h2>';
 
   const project1 = ProjectCard({
-    image: '/images/yogurt.png',
-    title: 'Project 1',
-    description: 'Description for project 1',
-    github: 'https://github.com/DulamaA/yogurt-project',
-    tags: ['HTML', 'SCSS', 'Layout'],
+    image: '/public/images/MarilynQuiz.jpg',
+    title: 'Marilyn Quiz',
+    description:
+      'Marilyn quiz is a group project, from the JavaScript and Agile course. It is built with TypeSCript and includes an accessibility reviewed webpage. The quiz contains 20 questions with 3 answer choices each, and every game round consists of 10 random questions. It also features a timer and a confirmation dialog showing how many answers were correct at the end.',
+    github: 'https://github.com/DulamaA/Marilyn-quiz-project',
   });
 
   const project2 = ProjectCard({
-    image: '/images/weather.png',
-    title: 'Weather App',
-    description: 'Weather info using live API data.',
-    github: 'https://github.com/DulamaA/weather-app',
-    tags: ['JavaScript', 'API', 'Responsive'],
+    image: '/public/images/Webbshop.jpg',
+    title: 'Gottfrids Muffin Factory',
+    description:
+      'Gottfrids Muffin Factory is a webshopp built with JavaScript, HTML and CSS. Includes real time cart updates, product sorting, a checkout form with validation, payment method selection, discount logic, and accessibility features. Built as part of a school project, following clean code practices using ESlint and Prettier.',
+    github: 'https://dulamaa.github.io/JS-Webbshop-project/',
   });
 
-  projects.appendChild(project1);
-  projects.appendChild(project2);
-  container.appendChild(projects);
+  const project3 = ProjectCard({
+    image: '/public/images/WeatherApp.jpg',
+    title: 'React Weather App',
+    description:
+      'React Weather App is a weather application built with React and TypeScript. It uses the OpenWeather API to fetch weather data and displays it in a user-friendly interface. The app includes features like location search, current weather conditions, and a 5-day forecast.',
+    github: 'https://github.com/DulamaA/react-weather-app',
+  });
+
+  projectsGrid.appendChild(project1);
+  projectsGrid.appendChild(project2);
+  projectsGrid.appendChild(project3);
+  container.appendChild(projectsGrid);
 
   //CONTACT section
   const contactSection = document.createElement('section');
   contactSection.id = 'contact';
   contactSection.innerHTML = `
   <h2>Contact Me</h2>
-  <p>  Email me at <a href="mailto:antoninanicolau@yahoo.com">antoninanicolau@yahoo.com</a></p>
-  `;
+  <p>
+  <i class="fas fa-envelope"></i>
+  <a href="mailto:antoninanicolau@yahoo.com">antoninanicolau@yahoo.com</a>
+</p>
+<p>
+  <i class="fab fa-linkedin"></i>
+  <a href="https://www.linkedin.com/in/antonina-dulama-682a2aa5/" target="_blank">LinkedIn</a>
+</p>
+<p>
+  <i class="fab fa-github"></i>
+  <a href="https://github.com/DulamaA" target="_blank">GitHub</a>
+</p>`;
   container.appendChild(contactSection);
 
   return container;
--- portfolio/src/components/Hero.ts
+++ portfolio/src/components/Hero.ts
@@ -2,9 +2,9 @@ const Hero = (): HTMLElement => {
   const section = document.createElement('section');
   section.className = 'hero';
   section.innerHTML = `
-    <h1>FrontEnd Developer Student Antonina Dulama</h1>
+    <h1>FrontEnd Developer Student - Antonina Dulama</h1>
     <p>I am studying at Medieinstitutet to become a frontend developer.</p>
-    <a href="#contact" class="btn">Contact me</a>
+    <a href="#contact" class="btn"> 👋 Let's connect</a>
   `;
   return section;
 };
